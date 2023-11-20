cce3c796f81052b6d2999c672203ddb2f62b57f8
Nauflars <46561459+Nauflars@users.noreply.github.com>
2023-11-20 18:14:47 +0100

    new version 0.2.5

--- main.js
+++ main.js
@@ -36,8 +36,6 @@ function createWindow () {
     win = null
   })
 }
-autoUpdater.logger = require("electron-log");
-autoUpdater.logger.transports.file.level = "info";
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
